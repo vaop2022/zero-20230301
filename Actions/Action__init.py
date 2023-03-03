54bcb47d94a0e29b6babb5991edef0116463028d
vrakitine <valeri.rakitine@gmail.com>
2023-03-03 14:08:25 -0500

    Updated setup(app)

---
+++ Actions/Action__init.py
@@ -0,0 +1,5 @@
+def Action__init(va_data, app_data):
+    app_data.set('Сумма...Sum', 0)
+    app_data.set('Индекс...i', 0)
+
+    va_data.set('Direction...direction', 'Direction_10')
--- VaConfigLocal.py
+++ VaConfigLocal.py
@@ -2,7 +2,10 @@ def setup(app):
 
     ### The  variables setting:
 
-    app.defineVariable('', 0)
+    app.defineVariable('Входящий массив...Arr', [])
+    app.defineVariable('Сумма...Sum', 0)
+    app.defineVariable('Индекс...i', 0)
+
 
     
     
